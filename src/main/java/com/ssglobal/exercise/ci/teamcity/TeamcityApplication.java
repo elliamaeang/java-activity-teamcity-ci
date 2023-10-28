4177d902e85e25dc5ccc93166b6548fa54849e46
Elliamae Ang <113429563+elliamaeang@users.noreply.github.com>
2023-10-29 03:36:58 +0800

    First commit

---
+++ src/main/java/com/ssglobal/exercise/ci/teamcity/TeamcityApplication.java
@@ -0,0 +1,13 @@
+package com.ssglobal.exercise.ci.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssglobal/exercise/ci/teamcity/TeamcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssglobal.exercise.ci.teamcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
